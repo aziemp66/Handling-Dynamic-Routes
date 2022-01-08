1553a77f9e90b4d8a4c34c1974cbada9d02561c6
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-08 08:14:58 +0700

    Hard Reset Again

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,50 +1,50 @@
-const express = require('express');
-const uuid = require('uuid');
+const express = require("express");
+const uuid = require("uuid");
 
-const resData = require('../util/restaurant-data');
+const resData = require("../util/restaurant-data");
 
 const router = express.Router();
 
-router.get('/restaurants', function (req, res) { 
-  const storedRestaurants = resData.getStoredRestaurants();
+router.get("/restaurants", function (req, res) {
+    const storedRestaurants = resData.getStoredRestaurants();
 
-  res.render('restaurants', {
-    numberOfRestaurants: storedRestaurants.length,
-    restaurants: storedRestaurants,
-  });
+    res.render("restaurants", {
+        numberOfRestaurants: storedRestaurants.length,
+        restaurants: storedRestaurants,
+    });
 });
 
-router.get('/restaurants/:id', function (req, res) {
-  const restaurantId = req.params.id;
-  const storedRestaurants = resData.getStoredRestaurants();
+router.get("/restaurants/:id", function (req, res) {
+    const restaurantId = req.params.id;
+    const storedRestaurants = resData.getStoredRestaurants();
 
-  for (const restaurant of storedRestaurants) {
-    if (restaurant.id === restaurantId) {
-      return res.render('restaurant-detail', { restaurant: restaurant });
+    for (const restaurant of storedRestaurants) {
+        if (restaurant.id === restaurantId) {
+            return res.render("restaurant-detail", { restaurant: restaurant });
+        }
     }
-  }
 
-  res.render('404');
+    res.render("404");
 });
 
-router.get('/recommend', function (req, res) { 
-  res.render('recommend');
+router.get("/recommend", function (req, res) {
+    res.render("recommend");
 });
 
-router.post('/recommend', function (req, res) {
-  const restaurant = req.body;
-  restaurant.id = uuid.v4();
-  const restaurants = resData.getStoredRestaurants();
+router.post("/recommend", function (req, res) {
+    const restaurant = req.body;
+    restaurant.id = uuid.v4();
+    const restaurants = resData.getStoredRestaurants();
 
-  restaurants.push(restaurant);
+    restaurants.push(restaurant);
 
-  resData.storeRestaurants(restaurants);
+    resData.storeRestaurants(restaurants);
 
-  res.redirect('/confirm');
+    res.redirect("/confirm");
 });
 
-router.get('/confirm', function (req, res) {
-  res.render('confirm');
+router.get("/confirm", function (req, res) {
+    res.render("confirm");
 });
 
 module.exports = router;
