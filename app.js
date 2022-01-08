c44b1fcc19efd723faa2984b4554b4dd480ea715
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-08 09:11:41 +0700

    Adding Query Parameters & Sorting Button

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ app.use("/", defaultRoutes);
 app.use("/", restaurantRoutes);
 
 app.use(function (req, res) {
-    res.render("404");
+    res.status(404).render("404");
 });
 
 app.use(function (error, req, res, next) {
-    res.render("500");
+    res.status(500).render("500");
 });
 
 app.listen(3000);
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -6,11 +6,32 @@ const resData = require("../util/restaurant-data");
 const router = express.Router();
 
 router.get("/restaurants", function (req, res) {
+    let order = req.query.order;
+    let nextOrder = "desc";
     const storedRestaurants = resData.getStoredRestaurants();
+
+    if (order !== "asc" && order !== "desc") {
+        order = "asc";
+    }
+
+    if (order == "desc") {
+        nextOrder = "asc";
+    }
+
+    storedRestaurants.sort(function (resA, resB) {
+        if (
+            (order == "asc" && resA.name > resB.name) ||
+            (order == "desc" && resB.name > resA.name)
+        ) {
+            return 1;
+        }
+        return -1;
+    });
 
     res.render("restaurants", {
         numberOfRestaurants: storedRestaurants.length,
         restaurants: storedRestaurants,
+        nextOrder: nextOrder,
     });
 });
 
